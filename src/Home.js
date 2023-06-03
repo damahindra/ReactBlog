cb60cf4f7fc76c31d9863b31486f41e22233a10f
ranggaandhito <91388757+ranggaandhito@users.noreply.github.com>
2023-06-03 19:46:35 +0700

    Added json server and fetch data to display

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,22 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {id: 1, title:'My new website', body:'Lorem Ipsum', author: 'Mario'},
-        {id: 2, title:'Welcome Party!', body:'Lorem Ipsum', author: 'Luigi'},
-        {id: 3, title:'Web Dev Top Tips', body:'Lorem Ipsum', author: 'Toad'},
-        {id: 4, title:'React 101', body:'Lorem Ipsum', author: 'Mario'}
-    ])
+    const [blogs, setBlogs] = useState(null);
 
-    const handleDelete = (id) => {
-        const filteredBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(filteredBlogs);
-    }
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            setBlogs(data);
+        })
+    }, []);
 
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title='All Blogs' handleDelete={handleDelete} />
-            {/* <BlogList blogs={blogs.filter((blog) => blog.author === "Mario")} title="Mario's Blogs" /> */}
+            {blogs && <BlogList blogs={blogs} title='All Blogs' />}
         </div>
      );
 }
