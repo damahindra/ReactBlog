721ff6946d7d28b0ad9d007c46cd798d24971090
ranggaandhito <91388757+ranggaandhito@users.noreply.github.com>
2023-06-03 21:28:39 +0700

    added error handling

--- src/Home.js
+++ src/Home.js
@@ -3,19 +3,32 @@ import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null)
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
+        setTimeout(() =>
+        fetch('http://localhost:8000/blog')
         .then(res => {
+            if (!res.ok) {
+                throw Error("Could not fetch data");
+            }
             return res.json();
         })
         .then(data => {
             setBlogs(data);
+            setIsPending(false)
         })
+        .catch(err => {
+            setError(err.message);
+            setIsPending(false);
+        }), 1000);
     }, []);
 
     return ( 
         <div className="home">
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title='All Blogs' />}
         </div>
      );
